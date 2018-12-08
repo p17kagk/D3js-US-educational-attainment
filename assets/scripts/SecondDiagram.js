7ced063c4b1d3c404adebf78bde2f1ce8c9b15f3
Andreas Kagkelaris Π2017203 <44092065+p17kagk@users.noreply.github.com>
2018-12-08 13:01:29 +0200

    Update SecondDiagram.js

--- assets/scripts/SecondDiagram.js
+++ assets/scripts/SecondDiagram.js
@@ -4,7 +4,7 @@ document.getElementById("First").style.display = "none";
 document.getElementById("pie-chart").style.display = "none";
 
 document.getElementById("Second").style.display = "block";
-document.getElementById("chart").style.display = "initial";
+document.getElementById("chart").style.display = "block";
 
 document.getElementById("Third").style.display = "none";
 document.getElementById("stacked-bar-chart").style.display = "none";
